a46af85f9d196db2ae2f474fce324b7699d0fd03
Val-Resh <80176765+Val-Resh@users.noreply.github.com>
2021-04-28 13:49:24 +0100

    Create removeZeroes.js

---
+++ removeZeroes.js
@@ -0,0 +1,26 @@
+/*PROBLEM:
+Write a function that takes an array of values and moves all elements that are zero to the end of the array, otherwise preserving the order of the array.
+The zero elements must also maintain the order in which they occurred.
+
+For example, the following array
+[7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14] 
+is transformed into 
+[7, 2, 3, 4, 6, 13, 78, 19, 14, 0, 0, 0, 0, 0, 0]
+
+Zero elements are defined by either 0 or "0". Some tests may include elements that are not number literals.
+You are NOT allowed to use any temporary arrays or objects. You are also not allowed to use any Array.prototype or Object.prototype methods.*/
+
+function removeZeros(array) {
+  let length = array.length;
+  for (let i = 0; i < length; i++){
+    if (array[i] === 0 || array[i] === "0"){
+      let temp = array[i];
+      for (let k = i--; k < array.length-1; k++){
+        array[k] = array[k + 1];
+      }
+      array[array.length-1] = temp;
+      length--;
+    }
+  }
+  return array;
+}
