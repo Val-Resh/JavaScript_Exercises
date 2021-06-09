dc2f9307916c531a6463e68838e48afc4f3a5a27
Val-Resh <80176765+Val-Resh@users.noreply.github.com>
2021-06-09 22:48:14 +0100

    Create evenTimesLast.js

---
+++ evenTimesLast.js
@@ -0,0 +1,14 @@
+/*PROBLEM:
+Given a sequence of integers, return the sum of all the integers that have an even index, multiplied by the integer at the last index.
+Indices in sequence start from 0.
+If the sequence is empty, you should return 0.*/
+
+function evenLast(numbers) {
+  let result = 0;
+  for (let i = 0; i < numbers.length; i += 2){
+    result += numbers[i];
+  }
+  if (result === 0) return 0;
+  result *= numbers[numbers.length - 1];
+  return result;
+}
