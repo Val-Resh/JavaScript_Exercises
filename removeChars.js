f523b0ff85e56d80d174ef710ccdd6327e06f7bb
Val-Resh <80176765+Val-Resh@users.noreply.github.com>
2021-06-12 22:01:15 +0100

    Create removeChars.js

---
+++ removeChars.js
@@ -0,0 +1,6 @@
+/*PROBLEM:
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string.
+You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+*/
+
+removeChar = str => str.substring(1, str.length - 1);
